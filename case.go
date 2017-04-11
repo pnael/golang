c490cbbb267a1a0df0385198857a79ae89778a63
Philippe NAEL <philippe.nael@gmail.com>
2017-04-11 18:51:22 +0200

    Essai

--- case.go
+++ case.go
@@ -5,15 +5,16 @@ import (
 	"runtime"
 )
 
+
+
 func main() {
 	fmt.Print("Go run on ")
-	switch os := runtime.GOOS; os {
+	switch os:=runtime.GOOS; os {
 	case "linux":
 		fmt.Println("Linux")
-	case "Windows":
-		fmt.Println("Windows")
 	default:
 		fmt.Println("other")
 	}
 
 }
+
